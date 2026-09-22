3a3c8bf2a5fdb70cf2056f81458f2fe76fd6e7ac
Khaled-AbHe <6320936@cegepmv.ca>
2026-09-22 11:08:38 -0400

    Created Review's model

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -1,1 +1,22 @@
-export class Review {}
+import { randomStringGenerator } from '@nestjs/common/internal';
+import { CreateReviewDto } from '../dto/create-review.dto.js';
+
+export class Review {
+  id!: string;
+  placeId!: string;
+  authorName!: string;
+  rating!: number;
+  comment!: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(dto: CreateReviewDto) {
+    this.id = 'rev_' + randomStringGenerator(); //temp
+    this.placeId = dto.placeId;
+    this.authorName = dto.authorName;
+    this.rating = dto.rating;
+    this.comment = dto.comment;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+}
